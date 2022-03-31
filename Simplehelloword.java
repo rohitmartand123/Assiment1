1b31916bb31ea0993ff374d17bae1db7db86ce5a
rohitmartand123 <rohitmartand274@gmail.com>
2022-03-31 13:57:44 +0530

    Added : Assiment1

---
+++ Simplehelloword.java
@@ -0,0 +1,17 @@
+package com.BridgeLabzAssiment1;
+/*
+ * @author Rohit
+ *  
+ */
+public class Simplehelloword {
+
+	public static void main(String[] args) {
+
+		     System.out.println("Hello Java");  
+		    }  
+		}  
+
+
+	
+
+
---
+++ Strings.java
@@ -0,0 +1,19 @@
+package com.BridgeLabzAssiment1;
+/*
+ *  @author Rohit
+ */
+public class Strings {
+
+	public static void main(String[] args) {
+
+        String style = "Bold";
+        String style2 = "Bold";
+
+        if(style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+
+	}
+
+}
